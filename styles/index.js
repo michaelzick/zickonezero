50047cb5fd4d6c5af19a3b4c07e63a816bfaf5cc
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-08-30 21:58:25 +0000

    Changed ParkMe to INRIX

--- styles/index.js
+++ styles/index.js
@@ -4,18 +4,18 @@ export const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   background-color: white;
+  background-image: url('/img/mountains-bg.jpg');
+  color: #fff;
+  text-align: center;
 `;
 
 export const Title = styled.h1`
-  color: #4e4e57;
   font-size: 6rem;
   margin: 0;
 `;
 
 export const SubTitle = styled.h2`
-  color: #4e4e57;
   margin: 0 0 3rem 0;
   line-height: 1.15;
   font-size: 3rem;
-  ${(props) => props.italic && 'font-style: italic;'}
 `;
