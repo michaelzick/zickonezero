da20881a1851022c442dee1ea756c854ea3e0992
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-09-03 22:29:36 +0000

    Bio changes

--- src/BioBoxContent.tsx
+++ src/BioBoxContent.tsx
@@ -7,9 +7,11 @@ const BioBoxContent = () => (
       <div className='text-wrapper'>
         <h2>Hi, I{"'"}m Michael.</h2>
         <br />
-        I{"'"}m a frontend engineer, coach, and cybersecurity enthusiast based in Los Angeles, CA. Most mornings you can find me at the beach or the gym earlier than probably anyone you know.
+        I{"'"}m a frontend engineer, coach, and cybersecurity enthusiast based in Los Angeles, CA. Most mornings you can find me at the beach or the gym earlier than most people you know.
         <br /><br />
-        You can see some samples of the projects I{"'"}ve build in the gallery above, code samples at <a href='https://github.com/michaelzick' target='_blank' rel='noreferrer'>GitHub</a>, and a full qualification on <a href='https://linkedin.com/in/michaelzick' target='_blank' rel='noreferrer'>LinkedIn</a>.
+        I have the CompTIA Security+ ce certification and over 15 years in IT. Additionally, I{"'"}ve led groups and individuals to personal and career success through coaching.
+        <br /><br />
+        You can see some samples of the projects I{"'"}ve built in the gallery above, code samples at <a href='https://github.com/michaelzick' target='_blank' rel='noreferrer'>GitHub</a>, and a full qualification on <a href='https://linkedin.com/in/michaelzick' target='_blank' rel='noreferrer'>LinkedIn</a>.
       </div>
       <Image width={300} height={336} layout='fixed' className='headshot' src='/img/headshot.jpeg' alt='headshot' style={{ borderRadius: 3 }} />
     </div>
