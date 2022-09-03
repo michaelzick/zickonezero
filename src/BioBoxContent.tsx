a71b8756c771231b370bdd1de538a7441c13617c
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-09-03 22:35:37 +0000

    More bio changes

--- src/BioBoxContent.tsx
+++ src/BioBoxContent.tsx
@@ -7,9 +7,9 @@ const BioBoxContent = () => (
       <div className='text-wrapper'>
         <h2>Hi, I{"'"}m Michael.</h2>
         <br />
-        I{"'"}m a frontend engineer, coach, and cybersecurity enthusiast based in Los Angeles, CA. Most mornings you can find me at the beach or the gym earlier than most people you know.
+        I{"'"}m a frontend engineer, coach, and cybersecurity enthusiast based in Los Angeles, CA. Most mornings you can find me at the beach or at the gym earlier than most people you know.
         <br /><br />
-        I have the CompTIA Security+ ce certification and over 15 years in IT. Additionally, I{"'"}ve led groups and individuals to personal and career success through coaching.
+        I have the CompTIA Security+ certification and over 15 years in IT. Additionally, I{"'"}ve led groups and individuals to personal and career success through coaching.
         <br /><br />
         You can see some samples of the projects I{"'"}ve built in the gallery above, code samples at <a href='https://github.com/michaelzick' target='_blank' rel='noreferrer'>GitHub</a>, and a full qualification on <a href='https://linkedin.com/in/michaelzick' target='_blank' rel='noreferrer'>LinkedIn</a>.
       </div>
