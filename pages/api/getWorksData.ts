d1764edbbb24cfc964b06893c44b2f2df42b5247
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-09-24 21:54:58 +0000

    Renamed handler to index (api)

--- pages/api/getWorksData.ts
+++ pages/api/getWorksData.ts
@@ -3,7 +3,7 @@ const getWorksData = async () => {
     const fetchData = await fetch(
       `${process.env.NODE_ENV === 'production' ?
         'https://www.zickonezero.com' :
-        'http://localhost:3000'}/api/handler`
+        'http://localhost:3000'}/api`
     );
     const dataJson = await fetchData.json();
     const { worksData } = dataJson;
