d17bb309383e6d5ac5ae70022155fb65b079f8bc
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-09-25 15:08:20 +0000

    Cleaned up worksDataSlice

--- src/worksDataSlice.ts
+++ src/worksDataSlice.ts
@@ -1,9 +1,7 @@
 import {
   createSlice,
-  PayloadAction,
 } from '@reduxjs/toolkit';
 import type { RootState } from './store';
-import worksData from '../pages/api/worksData.json';
 
 import type { WorksDataType } from '../src/types';
 
