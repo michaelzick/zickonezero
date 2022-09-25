b22558125ad173353f89d2528ca405d582705ee9
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-09-25 02:57:13 +0000

    Removed react-gtm-module; using <Script>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,28 +2,10 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../src/store';
-import { useEffect } from 'react';
-
-import TagManager from "react-gtm-module";
-
-const tagManagerArgs = {
-  gtmId: 'GTM-NWCBJRT'
-};
 
 function MyApp({
   Component, pageProps,
 }: AppProps) {
-  useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
-    TagManager.dataLayer({
-      dataLayer: {
-        event: 'pageview',
-        pagePath: '/',
-        pageTitle: 'Home',
-      },
-    });
-  }, []);
-
   return (
     <Provider store={store}>
       <Component {...pageProps} />
