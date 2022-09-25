188e27d48f71ee3e2c895b7a0398765e69ac7e2c
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-09-25 12:12:21 +0000

    Using new GA scripts

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,14 +29,15 @@ const Home: NextPage<WorksDataType> = (props) => {
         <HeadContents />
       </Head>
       <Script id='gtm' strategy="afterInteractive" dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5JHBZZX');`}}></Script>
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JHBZZX"
-        height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+          gtag('config', 'G-D9EDCV92S2');
+        `}}>
+      </Script>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-D9EDCV92S2"></Script>
       <MainContents />
       <FooterContents />
     </Container >
