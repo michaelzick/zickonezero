b63ebc7ed93e2c49b15e51ca3e3d98f5afd0cb54
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-11-12 10:07:14 +0000

    About text tweaks

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -30,13 +30,16 @@ const AboutContent = () => {
               <br />
               I{"'"}m a frontend engineer, coach, and cybersecurity enthusiast based in Los Angeles, CA.
               <br /><br />
-              I specialize in the React ecosystem, JavaScript, and server configurations including Docker. I also have experience with Python and PHP.
+              I specialize in the React ecosystem (Next.js, Redux, TypeScript), JavaScript, and server configurations including Docker.
+              I also have experience with Python, PHP, and UI/UX design.
               <br /><br />
-              I have the CompTIA Security+ certification and over 16 years in IT. Additionally, I{"'"}ve led groups and individuals to personal and career success through coaching.
+              I have the CompTIA Security+ certification and over 16 years in IT. Additionally, I{"'"}ve led groups and individuals to
+              personal and career success through coaching.
               <br /><br />
-              You can see some samples of the projects I{"'"}ve built in the <span className='underline'><Link href='/'>main gallery</Link></span>, code samples at{' '}
-              <a href='https://github.com/michaelzick' target='_blank' rel='noreferrer' className='underline'>GitHub</a>, and a full qualification on{' '}
-              <a href='https://linkedin.com/in/michaelzick' target='_blank' rel='noreferrer' className='underline'>LinkedIn</a>.
+              You can see some samples of the projects I{"'"}ve built in the <span className='underline'><Link href='/'>main gallery</Link></span>,
+              code samples at <a href='https://github.com/michaelzick' target='_blank' rel='noreferrer' className='underline'>GitHub</a>,
+              and a full qualification on <a href='https://linkedin.com/in/michaelzick' target='_blank' rel='noreferrer'
+                className='underline'>LinkedIn</a>.
             </div>
             <div className='headshot'>
               <Image src='/img/headshot.jpeg' alt='headshot' width='350' height='392' />
