8ec6827cf97a2427356459e0bb019c7773b05c64
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-11-13 21:19:59 +0000

    Implemented Rob's suggestions

--- src/components/LinkBoxContent.tsx
+++ src/components/LinkBoxContent.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link';
 
-import { LinkBox, Button } from '../../styles';
+import { LinkBox, NewTabGlyph } from '../../styles';
 
 const LinkBoxContent = () => (
   <LinkBox>
     <Link href='/about'>About</Link>
-    <a href='https://github.com/michaelzick' target='_blank' rel='noopener noreferrer'>GitHub</a>
-    <Button href='https://linkedin.com/in/michaelzick' target='_blank' rel='noopener noreferrer'>LinkedIn</Button>
+    <a href='https://github.com/michaelzick' target='_blank' rel='noopener noreferrer'>GitHub
+      <NewTabGlyph src='img/icons8-new-tab-50.png' alt='new tab icon' />
+    </a>
+    <a href='https://linkedin.com/in/michaelzick' target='_blank' rel='noopener noreferrer'>LinkedIn
+      <NewTabGlyph src='img/icons8-new-tab-50.png' alt='new tab icon' />
+    </a>
   </LinkBox>
 );
 
