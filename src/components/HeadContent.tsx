c29550f6ac39d8bdcc82dc3fdc502dc47e82000b
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-03-29 16:23:04 +0000

    Updated meta descriptions

--- src/components/HeadContent.tsx
+++ src/components/HeadContent.tsx
@@ -3,18 +3,18 @@ const HeadContent = () => (
     <link rel="shortcut icon" type="image/x-icon" href="/img/favicon_io/favicon.ico" />
     <link rel="canonical" href="https://www.zickonezero.com" />
     <meta name="title" content="ZICKONEZERO Product & Engineering" />
-    <meta name="description" content="Michael Zick is a Product & Engineering Manager, frontend developer, and all-around people person." />
+    <meta name="description" content="Product Management, Engineering Management, UX Design, Frontend Development, Git/DevOps, and Creative Direction." />
     <meta property="og:site_name" content="ZICKONEZERO Product & Engineering" />
     <meta property="og:title" content="ZICKONEZERO Product & Engineering" />
     <meta property="og:url" content="https://www.zickonezero.com" />
     <meta property="og:type" content="website" />
-    <meta property="og:description" content="Michael Zick is a Product & Engineering Manager, frontend developer, and all-around people person." />
+    <meta property="og:description" content="Product Management, Engineering Management, UX Design, Frontend Development, Git/DevOps, and Creative Direction." />
     <meta property="og:image" content="/img/headshot.jpeg" />
     <meta property="og:image:width" content="1500" />
     <meta property="og:image:height" content="996" />
     <meta itemProp="name" content="ZICKONEZERO Product & Engineering" />
     <meta itemProp="url" content="https://www.zickonezero.com" />
-    <meta itemProp="description" content="Michael Zick is a Product & Engineering Manager, frontend developer, and all-around people person." />
+    <meta itemProp="description" content="Product Management, Engineering Management, UX Design, Frontend Development, Git/DevOps, and Creative Direction." />
     <meta itemProp="thumbnailUrl" content="/img/headshot.jpeg" />
     <link rel="image_src" href="/img/headshot.jpeg" />
     <meta itemProp="image" content="/img/headshot.jpeg" />
@@ -22,7 +22,7 @@ const HeadContent = () => (
     <meta name="twitter:image" content="/img/headshot.jpeg" />
     <meta name="twitter:url" content="https://www.zickonezero.com" />
     <meta name="twitter:card" content="summary" />
-    <meta name="twitter:description" content="Michael Zick is a Product & Engineering Manager, frontend developer, and all-around people person." />
+    <meta name="twitter:description" content="Product Management, Engineering Management, UX Design, Frontend Development, Git/DevOps, and Creative Direction." />
     <link rel="icon" href="/img/favicon_io/favicon.ico" />
   </>
 );
