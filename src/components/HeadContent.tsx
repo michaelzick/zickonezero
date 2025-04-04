9803746d6b08de1bb0e85e7371e79e970f0b7de5
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-04-04 17:32:31 +0000

    New favicon and logo

--- src/components/HeadContent.tsx
+++ src/components/HeadContent.tsx
@@ -1,6 +1,6 @@
 const HeadContent = () => (
   <>
-    <link rel="shortcut icon" type="image/x-icon" href="/img/favicon_io/favicon.ico" />
+    <link rel="shortcut icon" type="image/x-icon" href="/img/favicon/favicon.ico" />
     <link rel="canonical" href="https://www.zickonezero.com" />
     <meta name="title" content="ZICKONEZERO Product & Engineering" />
     <meta name="description" content="Product Management, Engineering Management, UX Design, Frontend Development, Git/DevOps, and Creative Direction." />
@@ -9,21 +9,21 @@ const HeadContent = () => (
     <meta property="og:url" content="https://www.zickonezero.com" />
     <meta property="og:type" content="website" />
     <meta property="og:description" content="Product Management, Engineering Management, UX Design, Frontend Development, Git/DevOps, and Creative Direction." />
-    <meta property="og:image" content="/img/headshot.jpeg" />
+    <meta property="og:image" content="/img/squares/z10_logo.webp" />
     <meta property="og:image:width" content="1500" />
     <meta property="og:image:height" content="996" />
     <meta itemProp="name" content="ZICKONEZERO Product & Engineering" />
     <meta itemProp="url" content="https://www.zickonezero.com" />
     <meta itemProp="description" content="Product Management, Engineering Management, UX Design, Frontend Development, Git/DevOps, and Creative Direction." />
-    <meta itemProp="thumbnailUrl" content="/img/headshot.jpeg" />
-    <link rel="image_src" href="/img/headshot.jpeg" />
-    <meta itemProp="image" content="/img/headshot.jpeg" />
+    <meta itemProp="thumbnailUrl" content="/img/squares/z10_logo.webp" />
+    <link rel="image_src" href="/img/squares/z10_logo.webp" />
+    <meta itemProp="image" content="/img/squares/z10_logo.webp" />
     <meta name="twitter:title" content="ZICKONEZERO Product & Engineering" />
-    <meta name="twitter:image" content="/img/headshot.jpeg" />
+    <meta name="twitter:image" content="/img/squares/z10_logo.webp" />
     <meta name="twitter:url" content="https://www.zickonezero.com" />
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:description" content="Product Management, Engineering Management, UX Design, Frontend Development, Git/DevOps, and Creative Direction." />
-    <link rel="icon" href="/img/favicon_io/favicon.ico" />
+    <link rel="icon" href="/img/favicon/favicon.ico" />
   </>
 );
 
