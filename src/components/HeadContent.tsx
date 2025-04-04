d0baab97804279fc7efe021ece417349f8dc7fb7
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-04-04 17:28:39 +0000

    Changed perms and renamed logo

--- src/components/HeadContent.tsx
+++ src/components/HeadContent.tsx
@@ -9,17 +9,17 @@ const HeadContent = () => (
     <meta property="og:url" content="https://www.zickonezero.com" />
     <meta property="og:type" content="website" />
     <meta property="og:description" content="Product Management, Engineering Management, UX Design, Frontend Development, Git/DevOps, and Creative Direction." />
-    <meta property="og:image" content="/img/squares/Z10_Logo.webp" />
+    <meta property="og:image" content="/img/squares/z10_logo.webp" />
     <meta property="og:image:width" content="1500" />
     <meta property="og:image:height" content="996" />
     <meta itemProp="name" content="ZICKONEZERO Product & Engineering" />
     <meta itemProp="url" content="https://www.zickonezero.com" />
     <meta itemProp="description" content="Product Management, Engineering Management, UX Design, Frontend Development, Git/DevOps, and Creative Direction." />
-    <meta itemProp="thumbnailUrl" content="/img/squares/Z10_Logo.webp" />
-    <link rel="image_src" href="/img/squares/Z10_Logo.webp" />
-    <meta itemProp="image" content="/img/squares/Z10_Logo.webp" />
+    <meta itemProp="thumbnailUrl" content="/img/squares/z10_logo.webp" />
+    <link rel="image_src" href="/img/squares/z10_logo.webp" />
+    <meta itemProp="image" content="/img/squares/z10_logo.webp" />
     <meta name="twitter:title" content="ZICKONEZERO Product & Engineering" />
-    <meta name="twitter:image" content="/img/squares/Z10_Logo.webp" />
+    <meta name="twitter:image" content="/img/squares/z10_logo.webp" />
     <meta name="twitter:url" content="https://www.zickonezero.com" />
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:description" content="Product Management, Engineering Management, UX Design, Frontend Development, Git/DevOps, and Creative Direction." />
